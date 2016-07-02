67fdaac0a6d56c7b6826a5802704ca7a322ef6bd
kekuso <kekuso808@gmail.com>
2016-07-02 10:30:54 -1000

    Able to send server status and HTTP headers to client.

--- server.js
+++ server.js
@@ -7,25 +7,35 @@ var responseSuccess = 'HTTP/1.1 200 OK\n' +
                 'Date: ' + now.toUTCString() +
                 '\nServer: awesomeServer/1.0.0 (Ubuntu)\n';
 
+var serverHTTPHeaders;
+
 var server = net.createServer(function (socket) {
   var socketAddress = socket.address().address;
   var socketPort = socket.remotePort;
-  var firstLine = [];
-  var httpHeaders = [];
+  var clientFirstLine = [];
+  var clientHeaderLines = [];
+  var statusLine;
   console.log("CONNECTED: " + socketAddress + ":" + socketPort);
 
   socket.on('data', function(data) {
-    firstLine = data.toString().split(' ');
-    if(firstLine[0] === 'GET') {
+    clientFirstLine = data.toString().split(' ').splice(0, 3);
+    //console.log(clientFirstLine);
+    if(clientFirstLine[0] === 'GET') {
       console.log("Get detected.");
 
-      if(firstLine[1][0] === '/') {
-      console.log("/ detected.");
-      }
-      if(firstLine[2]) {
-        console.log(firstLine[2] + " detected.");
+      if(clientFirstLine[1][0] === '/') {
+        console.log("/ detected.");
+        // assume index.html always exists
+        if(clientFirstLine[2]) {
+          console.log(clientFirstLine[2] + " detected.");
+          statusLine = 'HTTP/1.1 200 OK';
+          socket.write(statusLine);
+          socket.write(serverHTTPHeaders);
+        }
       }
     }
+    clientHeaderLines = data.toString().split('\n').splice(1);
+    console.log(clientHeaderLines);
 
     console.log(('SERVER BCAST FROM ' + socketAddress + ":" + socket.remotePort + ": " + data).replace(/(\r\n|\n|\r)/gm,""));
   });
@@ -33,7 +43,10 @@ var server = net.createServer(function (socket) {
 
 server.listen(CONFIG.PORT, function () {
   var PORT = server.address().port;
+  var serverNow = new Date();
   console.log('Server listening on 127.0.0.1:' + CONFIG.PORT);
+  serverHTTPHeaders = 'Server: localhost\n' +
+                      'Date: ' + serverNow.toUTCString() + '\n';
 });
 
 server.on('error', function (err) {
